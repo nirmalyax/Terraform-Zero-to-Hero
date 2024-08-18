39ddfd3736bddc05dd4cf44369fc0888c9dbec9d
Nirmalya Mondal <nirmalya267@gmail.com>
2024-08-18 19:03:50 +0530

    first commit

---
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-04a81a99f5ec58529"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+    subnet_id = "subnet-0fe6c3c49fcf5ebdd"
+    key_name = "AWS-VPN"
+}
+
